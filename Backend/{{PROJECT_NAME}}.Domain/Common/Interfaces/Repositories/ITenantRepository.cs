06bd744a1a11ffa2bcf38aeefc1649bb48ecba9c
DESKTOP-CP1SS7U\Hp <alperenfaki@gmail.com>
2026-02-06 22:58:28 +0300

    feat : ITenant is added

--- Backend/{{PROJECT_NAME}}.Domain/Common/Interfaces/Repositories/ITenantRepository.cs
+++ Backend/{{PROJECT_NAME}}.Domain/Common/Interfaces/Repositories/ITenantRepository.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace {{PROJECT_NAME }}.Domain.Common.Interfaces.Repositories
+namespace {{PROJECT_NAME}}.Domain.Common.Interfaces.Repositories;
+
+public interface ITenantRepository : IRepository<Tenant, int>
 {
-    internal class ITenantRepository
-{
+    Task<Tenant> GetByNameAsync(string name);
+    Task<Tenant> GetByDomainAsync(string domain);
+    Task<Tenant> GetTenantWithUsersAsync(int tenantId);
+    Task<bool> NameExistsAsync(string name);
+    Task<bool> DomainExistsAsync(string domain);
 }
-}
+
